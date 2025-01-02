23adb35145f1ceef478cfbf9dcd177658e1ca97f
taiseen <taiseenx@gmail.com>
2025-01-02 22:18:29 +0600

    feat: qraphql apollo provoder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import { Toaster } from "react-hot-toast"
@@ -9,13 +10,22 @@ import './styles/index.css'
 const htmlRoot = document.getElementById('root');
 const reactRoot = createRoot(htmlRoot);
 
+const client = new ApolloClient({
+  // TODO => Update the uri on production
+  uri: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql", // the URL of our GraphQL server.
+  cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+  credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+});
+
 reactRoot.render(
 
   <StrictMode>
     <BrowserRouter>
       <GridBackground>
-        <App />
-        <Toaster />
+        <ApolloProvider client={client}>
+          <App />
+          <Toaster />
+        </ApolloProvider>
       </GridBackground>
     </BrowserRouter>
   </StrictMode>,
